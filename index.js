dab6bf57ae8993946c4cd2724e8826def9f0a958
Alex Kio <alexandermartelkio@gmail.com>
2021-11-26 14:06:31 -0500

    Create route to get all cards

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const PORT = process.env.PORT || 6666;
 app.use(cors());
 app.use(express.json());
 
+app.get("/cards", async (request, response) => {
+  try {
+    const allCards = await db.query("SELECT * FROM cards");
+    response.json(allCards.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`app has started on port ${PORT}`)
 });
