d9ce11f93389ef7f9aedac1c7e392d6e71f4df0b
Alex Kio <alexandermartelkio@gmail.com>
2022-01-02 15:35:45 -0500

    Further SOC refactoring

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,29 @@
+const express = require('express')
+const app = express()
+import router from './router'
+import cors from "cors"
+import helmet from "helmet"
+const compression = require('compression')
+const rateLimit = require('express-rate-limit')
 import * as dotenv from "dotenv"
 dotenv.config()
-import handlers from './handlers'
-import makeApp from './app'
 
-const app = makeApp(handlers)
+app.use(compression())
+app.use(helmet())
+app.use(cors())
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 200,
+})
+app.use(limiter)
+app.use(express.json())
 
 const PORT: number = parseInt(process.env.PORT as string) || 6565
 
-const server = app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.use(router)
 
-module.exports = server
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+}
+
+// module.exports = server
